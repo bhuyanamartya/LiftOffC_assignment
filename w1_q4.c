67c7830386053a41062f4f9b69b25e17790d950f
bhuyanamartya <99415664+bhuyanamartya@users.noreply.github.com>
2022-02-12 21:44:38 +0530

    Add files via upload

---
+++ w1_q4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+float r, D, C, A;
+printf("enter the radius of circle: \n");
+scanf("%f", &r);
+D = 2 * r;
+C = 2 * 3.14 * r;
+A = 3.14 * r * r;
+printf("Diameter is: %f", D);
+printf("\n");
+printf("Circumference is: %f", C);
+printf("\n");
+printf("Area is: %f", A);
+}
